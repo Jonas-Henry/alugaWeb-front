b238565f2b299cf23df19863f6f0ee165ce6011c
jonas-henry <jonashenrique57@gmail.com>
2025-06-14 21:42:38 -0300

    correções

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,13 +19,13 @@ const Login = () => {
           </div>
           <div>
             <form className="w-[344px]">
-              <label className="block mb-2 text-[#595959]">Email</label>
+              <label className="block mb-2 font-bold text-[#595959]">Email</label>
               <input
                 type="text"
                 className="bg-gray-500/15 rounded-[8px] h-[54px] w-[344px] outline-transparent border-[1px] border-[#D9D9D9]"
               />
 
-              <label className="block mt-[30px] mb-2 text-[#595959]">Senha</label>
+              <label className="block mt-[30px] mb-2 font-bold text-[#595959]">Senha</label>
               <input
                 type="password"
                 className="bg-gray-500/15 rounded-[8px] h-[54px] w-[344px] outline-transparent border-[1px] border-[#D9D9D9]"
@@ -41,7 +41,7 @@ const Login = () => {
                   Criar conta
                 </a>
               </div>
-              <button className="w-[344px] mt-[29px] h-[50px] bg-[#E04300] rounded-[8px] text-[#FFFFFF]">
+              <button className="w-[344px] h-[50px] bg-[#E04300] rounded-[8px] text-[#FFFFFF]">
                 Entrar
               </button>
             </form>
